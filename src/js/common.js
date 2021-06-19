3f3d20d7d63006b088f594775f0b6ddc15a99998
Yoojeen <sendtoeugeen@gmail.com>
2021-06-19 17:10:41 +0300

    moving divs row

--- src/js/common.js
+++ src/js/common.js
@@ -11,3 +11,29 @@
 				}, 2000)
 			},600)	
 	})
+
+	// перемещение блока
+	const featuresRow = document.querySelector('.business__features-row')
+	const features = document.querySelectorAll('.business__features-row p')
+	const leftEdge = features[0].getBoundingClientRect().left
+	features[0].classList.add('inposition')
+
+	for(let f of features){
+		let fLeftInit = f.getBoundingClientRect().left
+		f.dataset.initleft = fLeftInit
+		f.addEventListener('click', moveRow)
+	}
+
+	function moveRow(e){
+		const elem = e.target
+		const leftCoord = parseInt(elem.dataset.initleft)
+		// console.log(coords);
+		const distance = leftCoord - leftEdge
+		featuresRow.style.transform = `translateX(-${distance}px)`
+
+		features.forEach((f) => {
+			return f.classList.remove('inposition')
+		})
+
+		elem.classList.add('inposition')
+	}
